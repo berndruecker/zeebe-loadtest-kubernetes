97797e33ee059035734ab3dedf8786fd96342a79
Bernd Rücker <bernd.ruecker@camunda.com>
2018-11-20 15:21:54 +0100

    added elastic export of started metrics

--- zeebe-loadtest-starter/src/main/java/io/berndruecker/demo/zeebe/loadtest/starter/LoadtestStarterApplication.java
+++ zeebe-loadtest-starter/src/main/java/io/berndruecker/demo/zeebe/loadtest/starter/LoadtestStarterApplication.java
@@ -1,11 +1,14 @@
 package io.berndruecker.demo.zeebe.loadtest.starter;
 
+import java.net.URI;
 import java.util.List;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,39 +18,64 @@ import io.zeebe.client.api.commands.Workflow;
 @SpringBootApplication
 @Configuration
 public class LoadtestStarterApplication {
-  
+
   @Value("${zeebe.brokerContactPoint}")
   private String zeebeBrokerContactPoint;
-  
+
+  @Value("${elasticsearch.url}")
+  private String elasticsearchUrl;
+
   @Bean
   public ZeebeClient zeebe() {
     System.out.println("Connect to Zeebe at '" + zeebeBrokerContactPoint + "'...");
-    
+
     // Cannot yet use Spring Zeebe in current alpha
     ZeebeClient zeebeClient = ZeebeClient.newClientBuilder() //
         .brokerContactPoint(zeebeBrokerContactPoint) //
         .build();
-    
+
     // check if workflow is already deployed:
-    List<Workflow> workflows = zeebeClient.workflowClient()
-      .newWorkflowRequest()
-      .send().join()
-      .getWorkflows();
-    if (workflows.size()==0) {
+    List<Workflow> workflows = zeebeClient.workflowClient() //
+        .newWorkflowRequest() //
+        .send().join() //
+        .getWorkflows();
+    
+    boolean workflowDeployed = workflows.stream()
+        .anyMatch(workflow -> workflow.getBpmnProcessId().equals("sample-load-generation-workflow"));
+    
+    if (!workflowDeployed) {
       // Trigger deployment
       zeebeClient.workflowClient().newDeployCommand() //
-      .addResourceFromClasspath("sample-load-generation-workflow.bpmn") //
-      .send().join();      
+          .addResourceFromClasspath("sample-load-generation-workflow.bpmn") //
+          .send().join();
+      System.out.println("...deployed workflow definition successfully...");
     }
-    
+
     System.out.println("...connected.");
 
     return zeebeClient;
   }
 
+  @Bean
+  public RestHighLevelClient createElasticClient() {
+    try {
+      System.out.println("Connect to ElasticSearch at '" + elasticsearchUrl + "'...");
+  
+      URI uri = new URI(elasticsearchUrl); 
+      RestHighLevelClient client = new RestHighLevelClient( //
+          RestClient.builder(new HttpHost(uri.getHost(), uri.getPort(), uri.getScheme())));
+      
+      System.out.println("...connected.");
+      return client;
+    } catch (Exception ex) {
+      System.out.println("could not connect to elastic - ignoring");
+      ex.printStackTrace();
+      return null;
+    }
+  }
+
   public static void main(String[] args) throws Exception {
-    ConfigurableApplicationContext applicationContext = //
-        SpringApplication.run(LoadtestStarterApplication.class, args);    
+    SpringApplication.run(LoadtestStarterApplication.class, args);
   }
 
 }
--- zeebe-loadtest-starter/src/main/java/io/berndruecker/demo/zeebe/loadtest/starter/Starter.java
+++ zeebe-loadtest-starter/src/main/java/io/berndruecker/demo/zeebe/loadtest/starter/Starter.java
@@ -1,12 +1,19 @@
 package io.berndruecker.demo.zeebe.loadtest.starter;
 
+import java.net.InetAddress;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.annotation.PostConstruct;
 
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,11 +21,14 @@ import io.zeebe.client.ZeebeClient;
 
 @Component
 public class Starter {
-  
+
   static AtomicInteger amount = new AtomicInteger();
-  
+
   @Autowired
   private ZeebeClient zeebeClient;
+
+  @Autowired
+  private RestHighLevelClient elasticClient;
 
   @PostConstruct
   public void go() {
@@ -30,27 +40,54 @@ public class Starter {
 
   private void startInstance(String payload) {
     try {
-      zeebeClient.workflowClient()
-          .newCreateInstanceCommand()
-          .bpmnProcessId("sample-load-generation-workflow")
-          .latestVersion()
-          .payload(payload)
-          .send().join();
+      zeebeClient.workflowClient().newCreateInstanceCommand() //
+        .bpmnProcessId("sample-load-generation-workflow") //
+        .latestVersion() //
+        .payload(payload) //
+        .send().join();
     } catch (Exception ex) {
       ex.printStackTrace();
     }
     amount.incrementAndGet();
   }
 
-  private static void measureTime() {
+  private void measureTime() {
     Timer t = new Timer();
     t.schedule(new TimerTask() {
       @Override
       public void run() {
         Date date = new Date();
-        int andSet = amount.getAndSet(0);
-        System.out.println(date.toString() + ": " + andSet);
+        int count = amount.getAndSet(0);
+        sendToElastic(date, count);
+        System.out.println(date.toString() + ": " + count);
       }
+
     }, 0, 10000);
+  }
+
+  public void sendToElastic(Date date, int count) {
+    try {
+      if (elasticClient != null) {
+
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("timestamp", date);
+        jsonMap.put("type", "started");
+        jsonMap.put("hostname", InetAddress.getLocalHost().getHostName());
+        jsonMap.put("count", count);
+
+        IndexRequest indexRequest = new IndexRequest(//
+            "zeebe-load-metrics", //
+            "doc", //
+            UUID.randomUUID().toString()) //
+                .source(jsonMap);
+
+        elasticClient.index(indexRequest, RequestOptions.DEFAULT);
+      } else {
+        System.out.print(".Could not send metrics to elastic, not connected.");
+      }
+    } catch (Exception ex) {
+      System.out.println("Could not send metrics to elastic due to exception");
+      ex.printStackTrace();
+    }
   }
 }
